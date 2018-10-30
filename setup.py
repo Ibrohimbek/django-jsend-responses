8aaabe995dc16243a38ef38eb648632e528fc7a3
Ibrohim Ermatov <ibrohim@mysuperdispatch.com>
2018-10-30 14:07:35 +0500

    Create setup.py for PYPI

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="djangojsendresponses",
+    version="1.0.0",
+    url="https://github.com/Ibrohimbek/django-jsend-responses",
+    author="Ibrohim Ermatov",
+    author_email="ibrohimbek@gmail.com",
+    description="Ready Jsend specified response classes.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
