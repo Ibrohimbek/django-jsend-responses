334a89a8444679b0b30bb178378977df9bb6435c
Ibrohim Ermatov <ibrohim@mysuperdispatch.com>
2018-10-30 13:46:38 +0500

    Create responses

---
+++ django_jsend/responses.py
@@ -0,0 +1,66 @@
+from django.http import JsonResponse
+
+
+class SuccessResponse(JsonResponse):
+    status = 'success'
+
+    def __init__(self, object, status_code=200):
+        data = {
+            'status': self.status,
+            'data': {
+                'object': object,
+            }
+        }
+        super().__init__(data=data, status=status_code)
+
+
+class SuccessListResponse(JsonResponse):
+    status = 'success'
+
+    def __init__(self, objects, pagination=None):
+        data = {
+            'status': self.status,
+            'data': {
+                'objects': objects,
+                'pagination': pagination,
+            }
+        }
+        super().__init__(data=data)
+
+
+class FailResponse(JsonResponse):
+    status = 'fail'
+
+    def __init__(self, message='', status_code=404, details=None):
+        data = {
+            'status': self.status,
+            'data': {
+                'message': message,
+                'details': details or []
+            }
+        }
+        super().__init__(data=data, status=status_code)
+
+
+class ErrorResponse(JsonResponse):
+    status = 'error'
+
+    def __init__(self, message, status_code=400, error_code=None, data=None):
+        data = {
+            'status': self.status,
+            'code': error_code,
+            'message': message,
+            'data': data,
+        }
+        super().__init__(data=data, status=status_code)
+
+
+c = {
+    "status": "success",
+    "data": {
+        "object": {
+            "id": 1,
+            "name": "Fist object"
+        }
+     }
+}
